acec5130de204138ab1318722ec6c70a4843fbeb
Geoffroy <geoffroy.pudda@gmail.com>
2024-01-07 13:18:31 +0100

    Add nodejs configurated server

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 
+let express = require('express');
+
+let app = express();
+let server = app.listen(3000);
+
+app.use(express.static('public'));
+
 console.log("My server socket is running");
+console.log("localhost:3000");
+
+let socket = require('socket.io');
+
+let io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log('New connection: ' + socket.id);
+}
